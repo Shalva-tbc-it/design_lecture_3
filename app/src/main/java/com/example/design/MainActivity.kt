6b8f882139d262e081fe95e4f95b5b1b046931a0
shalva96 <gmshalva@gmail.com>
2023-10-27 17:45:33 +0400

    adden checking of username pass email

--- app/src/main/java/com/example/design/MainActivity.kt
+++ app/src/main/java/com/example/design/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.design
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.design.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,13 +16,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        onClick()
 
 
     }
 
-    fun isEmailValid(email: String): Boolean {
-        val emailRegex = "^[A-Za-z](.*)(@)(.+)(\\.)(.+)"
-        return email.matches(emailRegex.toRegex())
+    private fun onClick() {
+
+        binding.btnSave.setOnClickListener {
+            if (!isValidEmail(binding.edEmail.text)) {
+                Toast.makeText(this, "User Email Error!", Toast.LENGTH_LONG).show()
+            } else if (!isValidUserName(binding.edUserName.text.toString())) {
+                Toast.makeText(this, "user name Error", Toast.LENGTH_LONG).show()
+            }else if (!isStrongPassword(binding.edPass.text.toString())) {
+                Toast.makeText(this, "Password Error", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
     }
+
+    private fun isValidEmail(target: CharSequence?): Boolean {
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches()
+    }
+
+    private fun isValidUserName(target: String): Boolean {
+        val clearSpace = removeSpaces(target)
+        val userNameArray =
+            clearSpace.toCharArray()
+        return !TextUtils.isEmpty(target) && userNameArray.size >= 10
+    }
+
+    private fun removeSpaces(input: String): String {
+        return input.replace(" ", "")
+    }
+
+    private fun isStrongPassword(password: String): Boolean {
+        val pattern = Regex("^(?=.*[A-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@$!%*?&]{8,}\$")
+        return pattern.matches(password)
+    }
+
 }
